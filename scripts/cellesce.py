562b9760cf91e31d076764445f546d78a64560db
Craig Russell <ctr26@ebi.ac.uk>
2023-03-20 22:46:08 +0000

    Improving imports for cellesce

--- scripts/cellesce.py
+++ scripts/cellesce.py
@@ -3,27 +3,26 @@
 
 import subprocess
 
-subprocess.run("make get.data", shell=True)
+subprocess.run("make get.cellesce.data", shell=True)
 
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier, BaggingClassifier
-from sklearn.feature_selection import SelectFromModel, RFECV, RFE
-from sklearn.decomposition import PCA
-from sklearn.pipeline import Pipeline
-from sklearn import metrics
-from sklearn import model_selection
 import pathlib
 import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn import metrics, model_selection
+from sklearn.decomposition import PCA
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.feature_selection import RFE, RFECV, SelectFromModel
+from sklearn.pipeline import Pipeline
 
 import bioimage_phenotyping as bip
 from bioimage_phenotyping import Cellprofiler
 
 sns.set()
-from bioimage_phenotyping import Cellprofiler
-from bioimage_phenotyping import features
+from bioimage_phenotyping import Cellprofiler, features
 
 VARIABLES = ["Conc /uM", "Date", "Drug"]
 SAVE_FIG = True
@@ -82,6 +81,7 @@ features.plotting.df_to_fingerprints(
 plt.savefig(metadata("finger_prints.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 upper = np.nanmean(df.values.flatten()) + 2 * np.nanstd(df.values.flatten())
 lower = np.nanmean(df.values.flatten()) - 2 * np.nanstd(df.values.flatten())
@@ -115,7 +115,8 @@ plt.colorbar(cax=cax)
 plt.savefig(metadata("fingerprints_cells.pdf"), bbox_inches="tight")
 plt.show()
 plt.close()
-
+print("OK")
+print("OK")
 # %% Cell and drug fingerprints
 
 sns.set()
@@ -147,6 +148,7 @@ plt.colorbar(cax=cax)
 plt.savefig(metadata("fingerprints_drugs.pdf"), bbox_inches="tight")
 plt.show()
 plt.close()
+print("OK")
 
 
 # %%
@@ -164,7 +166,7 @@ sns.barplot(
 plt.tight_layout()
 plt.savefig(metadata("importance_median_control_points.pdf"))
 plt.show()
-plt.close()
+print("OK")
 # %%
 # sns.barplot(
 #     y="Feature", x="Cumulative Importance",
@@ -207,6 +209,7 @@ if SAVE_FIG:
     plt.savefig(metadata("Cell_predictions_image_vs_nuclei.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 # %% Could do better with median per imagenumber
 # plot = sns.catplot(
@@ -269,6 +272,7 @@ if SAVE_FIG:
     plt.savefig(metadata("Cell_predictions_organoid.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 plot = sns.catplot(
     # x="Kind",
@@ -292,6 +296,7 @@ if SAVE_FIG:
     plt.savefig(metadata("Drug_predictions_per_organoid.pdf"))
 plt.show()
 plt.close()
+print("OK")
 
 # %%
 sns.catplot(
@@ -314,6 +319,7 @@ if SAVE_FIG:
     plt.savefig(metadata("Organoid_Summary.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 sns.catplot(
     y="Conc /uM",
@@ -330,6 +336,7 @@ plt.tight_layout()
 plt.savefig(metadata("Nuclei_Summary.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 plot = sns.histplot(
     x="Date",
@@ -347,6 +354,7 @@ plt.tight_layout()
 plt.savefig(metadata("Date_summary_organoids.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %%
 
 plot = sns.histplot(
@@ -363,8 +371,9 @@ plot = sns.histplot(
 plt.xticks(rotation=90)
 plt.tight_layout()
 plt.savefig(metadata("Date_summary_organoids.pdf"))
-plt.show()
+plt.show
 plt.close()
+print("OK")
 # %%
 df_new = df.bip.select_features().bip.grouped_median("ObjectNumber")
 
@@ -385,6 +394,7 @@ plot = sns.catplot(
 ).set_xticklabels(rotation=45)
 plt.show()
 plt.close()
+print("OK")
 # %%
 
 plot = sns.catplot(
@@ -413,187 +423,10 @@ if SAVE_FIG:
     plt.savefig(metadata("Cell_predictions_image_vs_nuclei.pdf"))
 plt.show()
 plt.close()
+print("OK")
 # %% Concentration dependent study
 print()
-#
 
-# sns.clustermap(
-#     (
-#         df.bip.grouped_median("ObjectNumber")
-#         .bip.keeplevel(["Cell", "Drug"])
-#         .T.corr()
-#     )
-# )
-# plt.show()
-# sns.clustermap(
-#     (df.bip.grouped_median("ObjectNumber")
-#      .bip.keeplevel(["Cell"])
-#      .T.corr())
-# )
-# plt.show()
-# sns.clustermap(
-#     (df.bip.grouped_median("ObjectNumber")
-#      .bip.keeplevel(["Drug"])
-#      .T.corr())
-# )
-# plt.show()
+#%% Section on predicting similarity between 0 and 1 conc
 
-
-# # %%
-# # model = Pipeline([
-# #                     ("PCA", PCA()),
-# #                     ("modelselect", SelectFromModel(RandomForestClassifier())),
-# #                     ("RandomForest",RandomForestClassifier())
-# #                   ])
-# model = RandomForestClassifier()
-# # model = Pipeline([
-# #                     ("PCA", PCA()),
-# #                     ("RandomForest",RandomForestClassifier())
-# #                   ])
-
-# importance = df.bip.feature_importances(RandomForestClassifier(), variable="Cell")
-# print("Nuclei Cell")
-# importance = df.bip.grouped_median().bip.feature_importances(
-#     model, variable="Cell"
-# )
-# print("Organoid Cell")
-
-# importance = df.bip.feature_importances(RandomForestClassifier(), variable="Drug")
-# print("Nuclei Drug")
-
-# importance = df.bip.grouped_median().bip.feature_importances(model, variable="Drug")
-# print("Organoid Drug")
-
-
-# # fig_dims = (8, 6)
-
-# sns.barplot(
-#     y="Feature", x="Cumlative importance",
-#     data=df.bip.feature_importances(variable="Cell")
-# )
-# plt.tight_layout()
-
-
-# %%
-
-# importance = df.classification_report(model)
-# # %%
-# VARS = ["Cell", "Drug", "Conc /uM"]
-# variable = VARS[1]
-# # for VAR in VARS:
-# def get_score_report(df, variable="Cell"):
-#     # labels, uniques = pd.factorize(df.reset_index()[variable])
-#     X, y = df, list(df.index.get_level_values(variable))
-#     uniques = df.index.get_level_values(variable).to_series().unique()
-#     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
-#     X_train, X_test, y_train, y_test = df.bip.train_test_split(variable)
-#     model.fit(X_train, y_train)
-#     # y_pred = pd.Series(model.predict(X_test), index=X_test.index)
-
-#     report = pd.DataFrame(
-#         metrics.classification_report(y_test, model.predict(X_test), output_dict=True)
-#     ).drop(["accuracy", "macro avg", "weighted avg"], axis=1)
-#     report_tall = (
-#         report.rename_axis("Metric")
-#         .melt(
-#             # id_vars="Metric",
-#             var_name="Kind",
-#             value_name="Score",
-#             ignore_index=False,
-#         )
-#         .assign(**{"Variable": variable})
-#         .reset_index()
-#     )
-#     report_tall["Cohen Kappa"] = metrics.cohen_kappa_score(
-#         y_test, model.predict(X_test)
-#     )
-#     return report_tall
-
-
-# %%
-# data = report_df.set_index("Variable").xs("Drug")
-# %%
-# data=pd.concat([
-#         get_score_report(df.bip.grouped_median("ObjectNumber"),"Cell").assign(**{"Population type": "Organoid"}),
-#         get_score_report(df,"Cell").assign(**{"Population type": "Nuclei"})
-#         ]),
-
-# df.bip.grouped_median("ObjectNumber"),"Cell").groupby(level="Cell").apply(get_score_report)
-# plot = sns.catplot(
-#     x="Kind",
-#     y="Score",
-#     col="Metric",
-#     # row="Cell",
-#     ci=None,
-#     # hue="Metric",
-#     data=report_df.xs("Drug", level="Variable").reset_index(),
-#     sharey=False,
-#     kind="bar",
-# ).set_xticklabels(rotation=45)
-# %%
-
-
-# # a.bip.test_fun()
-
-# a.groupby("col1").apply(lambda d: d.bip.test_fun())
-
-# %%
-
-# df.pipe(get_score_report,variable)
-# (df
-#  .groupby(level=["Conc /uM"])
-#  .apply(get_score_report,)
-# )
-
-# report_df = [df.groupby(level=variable).apply(
-#     lambda x: get_score_report(x,variable)
-#     )
-#  for variable in [VARS[1]]];report_df
-
-# report_df = [df.groupby(level=variable).apply(
-#     lambda x: get_score_report(x,variable)
-#     )
-#  for variable in [VARS[0]]];report_df
-
-# report_df = [df.groupby(level=variable).apply(
-#     lambda x: get_score_report(x,variable)
-#     )
-#  for variable in [VARS[2]]];report_df
-# %%
-# return report_tall
-# a = get_score_report(df,"Cell")
-# b = get_score_report(df,"Drug")
-# c = get_score_report(df,"Conc /uM")
-# report_df = pd.concat(
-#     [
-#         get_score_report(df.bip.grouped_median("ObjectNumber"), variable).assign(
-#             **{"Population type": "Median"}
-#         )
-#         for variable in VARS
-#     ]
-# ).set_index(["Metric", "Kind", "Variable", "Population type"])
-
-# report_df = pd.concat(
-#         get_score_report(df.bip.grouped_median("ObjectNumber"), "Cell").assign(
-#             **{"Population type": "Median"}
-#         )
-# ).set_index(["Metric", "Kind", "Variable", "Population type"])
-# %%
-
-# df.bip.grouped_median("ObjectNumber").apply()
-
-# (pd.concat([
-#     df.assign(**{"Population type": "Organoid"}),
-#     df.bip.grouped_median("ObjectNumber")
-#     ])
-#     .apply(lambda x: get_score_report(x,"Cell"))
-# )
-
-# df.bip.grouped_median("ObjectNumber").apply(lambda x: get_score_report(x,"Cell"))
-
-# pd.concat([
-# get_score_report(df.bip.grouped_median("ObjectNumber"),"Cell").assign(**{"Population type": "Organoid"}),
-# get_score_report(df,"Cell").assign(**{"Population type": "Nuclei"})
-# ])
-# get_score_report(df, "Cell").assign(**{"Population type": "Median"})
-# get_score_report(df.bip.grouped_median("ObjectNumber"), "Cell")
+df
