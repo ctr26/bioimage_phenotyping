b87c2f045a7688a7f0c6749c35dad31bb2350617
Craig Russell <ctr26@ebi.ac.uk>
2023-03-30 21:47:18 +0100

    Moving shap (needs typing)

--- bioimage_phenotyping/dataset.py
+++ bioimage_phenotyping/dataset.py
@@ -56,13 +56,6 @@ import pandas as pd
 
 
 
-def get_shap_df(shaps):
-    return (
-        pd.DataFrame(shaps.values, columns=shaps.feature_names)
-        .rename_axis("Sample")
-        .reset_index()
-        .melt(id_vars="Sample", var_name="Feature", value_name="Shap Value")
-    )
 
 
 def get_scoring_df(
