ea5fac59394c9e92cbe4244e981f4132aaaac8e8
Craig Russell <ctr26@ebi.ac.uk>
2023-03-30 21:45:42 +0100

    Moving cellprofiler to it's own file

---
+++ bioimage_phenotyping/dataset/cellprofiler.py
@@ -0,0 +1,519 @@
+BAD_COLS = {
+    "Metadata_ChannelNumber_Organoid",
+    "Metadata_FileLocation_Organoid",
+    "Metadata_Frame_Organoid",
+    "Metadata_Plate_Organoid",
+    "Metadata_Series_Organoid",
+    "Metadata_Site_Organoid",
+    "Metadata_Well_Organoid",
+    "Children_NucleiObjects_Count",
+    "Number_Object_Number_Organoid",
+    "Parent_NucleiObjectsPrimary",
+    "Metadata_ChannelNumber_Nuclei",
+    "Metadata_FileLocation_Nuclei",
+    "Metadata_Frame_Nuclei",
+    "Metadata_Plate_Nuclei",
+    "Metadata_Series_Nuclei",
+    "Metadata_Site_Nuclei",
+    "Metadata_Well_Nuclei",
+    "Metadata_ChannelNumber",
+    "Metadata_FileLocation",
+    "Metadata_Frame",
+    "Metadata_Plate",
+    "Metadata_Series",
+    "Metadata_Site",
+    "Metadata_Well",
+    "Channel_Channels",
+    "Count_MergedCellObjects",
+    "Count_NucleiObjects",
+    "Count_NucleiObjectsPrimary",
+    "ExecutionTime_01Images",
+    "ExecutionTime_02Metadata",
+    "ExecutionTime_03NamesAndTypes",
+    "ExecutionTime_04Groups",
+    "ExecutionTime_05ColorToGray",
+    "ExecutionTime_06RescaleIntensity",
+    "ExecutionTime_07IdentifyPrimaryObjects",
+    "ExecutionTime_08SplitOrMergeObjects",
+    "ExecutionTime_09IdentifyPrimaryObjects",
+    "ExecutionTime_10RelateObjects",
+    "ExecutionTime_11MeasureObjectIntensity",
+    "ExecutionTime_12MeasureObjectSizeShape",
+    "ExecutionTime_13MeasureGranularity",
+    "ExecutionTime_14MeasureTexture",
+    "ExecutionTime_15MeasureImageAreaOccupied",
+    "ExecutionTime_19OverlayOutlines",
+    "ExecutionTime_20SaveImages",
+    "ExecutionTime_17MeasureGranularity",
+    "FileName_Channels",
+    "FileName_OrigOverlay",
+    "Frame_Channels",
+    "Group_Index",
+    "Group_Number",
+    "Height_Channels",
+    "ImageSet_ImageSet",
+    "MD5Digest_Channels",
+    "Metadata_ChannelNumber",
+    "Metadata_FileLocation",
+    "Metadata_Frame",
+    "Metadata_Plate",
+    "Metadata_Series",
+    "Metadata_Site",
+    "Metadata_Well",
+    "ModuleError_01Images",
+    "ModuleError_02Metadata",
+    "ModuleError_03NamesAndTypes",
+    "ModuleError_04Groups",
+    "ModuleError_05ColorToGray",
+    "ModuleError_06RescaleIntensity",
+    "ModuleError_07IdentifyPrimaryObjects",
+    "ModuleError_08SplitOrMergeObjects",
+    "ModuleError_09IdentifyPrimaryObjects",
+    "ModuleError_10RelateObjects",
+    "ModuleError_11MeasureObjectIntensity",
+    "ModuleError_12MeasureObjectSizeShape",
+    "ModuleError_13MeasureGranularity",
+    "ModuleError_14MeasureTexture",
+    "ModuleError_15MeasureImageAreaOccupied",
+    "ModuleError_19OverlayOutlines",
+    "ModuleError_20SaveImages",
+    "PathName_Channels",
+    "PathName_OrigOverlay",
+    "ProcessingStatus",
+    "Scaling_Channels",
+    "Series_Channels",
+    "URL_Channels",
+    "URL_OrigOverlay",
+    "Width_Channels",
+    "Metadata_C",
+    "Metadata_SizeC",
+    "Metadata_SizeT",
+    "Metadata_SizeX",
+    "Metadata_SizeY",
+    "Metadata_SizeZ",
+    "Metadata_Z",
+    "Channel_Raw",
+    "Channel_background",
+    "Channel_nuclei_border",
+    "Channel_nuclei_mask",
+    "Count_nuclei_mask_borders_scaled_object",
+    "Count_nuclei_mask_scaled_object",
+    "ExecutionTime_05RescaleIntensity",
+    "ExecutionTime_07RescaleIntensity",
+    "ExecutionTime_08RescaleIntensity",
+    "ExecutionTime_09ImageMath",
+    "ExecutionTime_10RescaleIntensity",
+    "ExecutionTime_11IdentifyPrimaryObjects",
+    "ExecutionTime_12IdentifySecondaryObjects",
+    "ExecutionTime_14MeasureObjectIntensity",
+    "ExecutionTime_15MeasureObjectSizeShape",
+    "ExecutionTime_16MeasureGranularity",
+    "ExecutionTime_17MeasureTexture",
+    "ExecutionTime_18MeasureImageAreaOccupied",
+    "ExecutionTime_19MeasureImageQuality",
+    "ExecutionTime_23OverlayOutlines",
+    "ExecutionTime_24Tile",
+    "ExecutionTime_25SaveImages",
+    "ExecutionTime_26SaveImages",
+    "Frame_Raw",
+    "Frame_background",
+    "Frame_nuclei_border",
+    "Frame_nuclei_mask",
+    "Height_Raw",
+    "Height_background",
+    "Height_nuclei_border",
+    "Height_nuclei_mask",
+    "Metadata_C_Image",
+    "Metadata_Series_Image",
+    "Metadata_SizeC_Image",
+    "Metadata_SizeT_Image",
+    "Metadata_SizeX_Image",
+    "Metadata_SizeY_Image",
+    "Metadata_SizeZ_Image",
+    "Metadata_Z_Image",
+    "ModuleError_05RescaleIntensity",
+    "ModuleError_07RescaleIntensity",
+    "ModuleError_08RescaleIntensity",
+    "ModuleError_09ImageMath",
+    "ModuleError_10RescaleIntensity",
+    "ModuleError_11IdentifyPrimaryObjects",
+    "ModuleError_12IdentifySecondaryObjects",
+    "ModuleError_14MeasureObjectIntensity",
+    "ModuleError_15MeasureObjectSizeShape",
+    "ModuleError_16MeasureGranularity",
+    "ModuleError_17MeasureTexture",
+    "ModuleError_18MeasureImageAreaOccupied",
+    "ModuleError_19MeasureImageQuality",
+    "ModuleError_23OverlayOutlines",
+    "ModuleError_24Tile",
+    "ModuleError_25SaveImages",
+    "ModuleError_26SaveImages",
+    "Scaling_Raw",
+    "Scaling_background",
+    "Scaling_nuclei_border",
+    "Scaling_nuclei_mask",
+    "Series_Raw",
+    "Series_background",
+    "Series_nuclei_border",
+    "Series_nuclei_mask",
+    "Channel_Raw",
+    "Channel_background",
+    "Channel_nuclei_border",
+    "Channel_nuclei_mask",
+    "Count_nuclei_mask_borders_scaled_object",
+    "Count_nuclei_mask_scaled_object",
+    "ExecutionTime_05RescaleIntensity",
+    "ExecutionTime_07RescaleIntensity",
+    "ExecutionTime_08RescaleIntensity",
+    "ExecutionTime_09ImageMath",
+    "ExecutionTime_10RescaleIntensity",
+    "ExecutionTime_11IdentifyPrimaryObjects",
+    "ExecutionTime_12IdentifySecondaryObjects",
+    "ExecutionTime_14MeasureObjectIntensity",
+    "ExecutionTime_15MeasureObjectSizeShape",
+    "ExecutionTime_16MeasureGranularity",
+    "ExecutionTime_17MeasureTexture",
+    "ExecutionTime_18MeasureImageAreaOccupied",
+    "ExecutionTime_19MeasureImageQuality",
+    "ExecutionTime_23OverlayOutlines",
+    "ExecutionTime_24Tile",
+    "ExecutionTime_25SaveImages",
+    "ExecutionTime_26SaveImages",
+    "Frame_Raw",
+    "Frame_background",
+    "Frame_nuclei_border",
+    "Frame_nuclei_mask",
+    "Height_Raw",
+    "Height_background",
+    "Height_nuclei_border",
+    "Height_nuclei_mask",
+    "Metadata_C_Image",
+    "Metadata_Series_Image",
+    "Metadata_SizeC_Image",
+    "Metadata_SizeT_Image",
+    "Metadata_SizeX_Image",
+    "Metadata_SizeY_Image",
+    "Metadata_SizeZ_Image",
+    "Metadata_Z_Image",
+    "ModuleError_05RescaleIntensity",
+    "ModuleError_07RescaleIntensity",
+    "ModuleError_08RescaleIntensity",
+    "ModuleError_09ImageMath",
+    "ModuleError_10RescaleIntensity",
+    "ModuleError_11IdentifyPrimaryObjects",
+    "ModuleError_12IdentifySecondaryObjects",
+    "ModuleError_14MeasureObjectIntensity",
+    "ModuleError_15MeasureObjectSizeShape",
+    "ModuleError_16MeasureGranularity",
+    "ModuleError_17MeasureTexture",
+    "ModuleError_18MeasureImageAreaOccupied",
+    "ModuleError_19MeasureImageQuality",
+    "ModuleError_23OverlayOutlines",
+    "ModuleError_24Tile",
+    "ModuleError_25SaveImages",
+    "ModuleError_26SaveImages",
+    "Scaling_Raw",
+    "Scaling_background",
+    "Scaling_nuclei_border",
+    "Scaling_nuclei_mask",
+    "Series_Raw",
+    "Series_background",
+    "Series_nuclei_border",
+    "Series_nuclei_mask",
+    "Width_Raw",
+    "Width_background",
+    "Width_nuclei_border",
+    "Width_nuclei_mask",
+    "Intensity_UpperQuartileIntensity_Raw_scaled",
+    "Location_CenterMassIntensity_X_Raw_scaled",
+    "Location_CenterMassIntensity_Y_Raw_scaled",
+    "Location_CenterMassIntensity_Z_Raw_scaled",
+    "Location_Center_X",
+    "Location_Center_Y",
+    "Location_Center_Z",
+    "Location_MaxIntensity_X_Raw_scaled",
+    "Location_MaxIntensity_Y_Raw_scaled",
+    "Location_MaxIntensity_Z_Raw_scaled",
+    "Number_Object_Number",
+    "Channel_Raw",
+    "Channel_background",
+    "Channel_nuclei_border",
+    "Channel_nuclei_mask",
+    "Count_nuclei_mask_borders_scaled_object",
+    "Count_nuclei_mask_scaled_object",
+    "ExecutionTime_05RescaleIntensity",
+    "ExecutionTime_07RescaleIntensity",
+    "ExecutionTime_08RescaleIntensity",
+    "ExecutionTime_09ImageMath",
+    "ExecutionTime_10RescaleIntensity",
+    "ExecutionTime_11IdentifyPrimaryObjects",
+    "ExecutionTime_12IdentifySecondaryObjects",
+    "ExecutionTime_14MeasureObjectIntensity",
+    "ExecutionTime_15MeasureObjectSizeShape",
+    "ExecutionTime_16MeasureGranularity",
+    "ExecutionTime_17MeasureTexture",
+    "ExecutionTime_18MeasureImageAreaOccupied",
+    "ExecutionTime_19MeasureImageQuality",
+    "ExecutionTime_23OverlayOutlines",
+    "ExecutionTime_24Tile",
+    "ExecutionTime_25SaveImages",
+    "ExecutionTime_26SaveImages",
+    "Frame_Raw",
+    "Frame_background",
+    "Frame_nuclei_border",
+    "Frame_nuclei_mask",
+    "ExecutionTime_01Images",
+    "ExecutionTime_02Metadata",
+    "ExecutionTime_03NamesAndTypes",
+    "ExecutionTime_04Groups",
+    "ExecutionTime_06RescaleIntensity",
+    "ExecutionTime_13MeasureGranularity",
+    "ExecutionTime_14MeasureImageAreaOccupied",
+    "ExecutionTime_15MeasureImageIntensity",
+    "ExecutionTime_17MeasureImageQuality",
+    "ExecutionTime_19MeasureObjectIntensity",
+    "ExecutionTime_20MeasureObjectIntensityDistribution",
+    "ExecutionTime_23MeasureObjectSizeShape",
+    "ExecutionTime_26MeasureTexture",
+}
+
+
+import pandas as pd
+import numpy as np
+import json
+
+
+
+def zero_is_control(df):
+    index_headers = list(df.index.names)
+    df = df.reset_index()
+    # index = df.index.to_frame()
+    df["Drug"] = df["Drug"].where(df["Conc /uM"] != 0, other="Control")
+    return df.set_index(index_headers)
+
+
+def force_numeric(df):
+    return (
+        df.select_dtypes("number")
+        .apply(pd.to_numeric)
+        .select_dtypes("number")
+        .replace([np.inf, -np.inf], np.nan)
+        .dropna(axis=1)
+        # .replace([np.inf, -np.inf], np.nan)
+        # .dropna(axis=1)
+        .sample(frac=1)
+    )
+
+
+def nuclei_primary_filter(df):
+    return df.filter(regex=r"^((?!NucleiObjectsPrimary).)*$")
+
+
+def drop_bad_cols_by_file(df, bad_cols_file):
+    if bad_cols_file == "":
+        return df
+    with open(bad_cols_file, "r") as f:
+        # print(*raw_df_indexed.columns,sep='\n')
+        return df.pipe(drop_bad_cols, json.load(f))
+
+
+def drop_bad_cols(df, bad_cols):
+    return df.drop(columns=bad_cols, errors="ignore")
+
+
+def drop_bad_index(df):
+    return df.reindex(df.index.dropna())
+
+
+
+def zero_is_control(df):
+    index_headers = list(df.index.names)
+    df = df.reset_index()
+    # index = df.index.to_frame()
+    df["Drug"] = df["Drug"].where(df["Conc /uM"] != 0, other="Control")
+    return df.set_index(index_headers)
+
+
+def force_numeric(df):
+    return (
+        df.select_dtypes("number")
+        .apply(pd.to_numeric)
+        .select_dtypes("number")
+        .replace([np.inf, -np.inf], np.nan)
+        .dropna(axis=1)
+        # .replace([np.inf, -np.inf], np.nan)
+        # .dropna(axis=1)
+        .sample(frac=1)
+    )
+
+
+def nuclei_primary_filter(df):
+    return df.filter(regex=r"^((?!NucleiObjectsPrimary).)*$")
+
+
+def drop_bad_cols_by_file(df, bad_cols_file):
+    if bad_cols_file == "":
+        return df
+    with open(bad_cols_file, "r") as f:
+        # print(*raw_df_indexed.columns,sep='\n')
+        return df.pipe(drop_bad_cols, json.load(f))
+
+
+def drop_bad_cols(df, bad_cols):
+    return df.drop(columns=bad_cols, errors="ignore")
+
+
+def drop_bad_index(df):
+    return df.reindex(df.index.dropna())
+
+
+
+class Cellprofiler(pd.DataFrame):
+    def __init__(
+        self,
+        *args,
+        data_folder=None,
+        nuclei_path="Secondary.csv",
+        image_path=None,
+        organoid_path=None,
+        object_headers=["ImageNumber", "ObjectNumber"],
+        filename_headers=["Date", "Drug", "Cell", "Replicate", "Conc /uM"],
+        # regex_pattern=r"[\/\\](?P<Date>[\d]+)_.+_(?P<Cell>ISO[\d]+)_(?P<Drug>[A-Za-z0-9]+)_(?P<Concentration>[\d\w_-]+uM)(?:.+Position_(?P<Position>[\d]))?",
+        regex_pattern=r"(?P<Date>([\d]{4}\-[\d]{2}\-[\d]{2})).+_(?P<Cell>ISO[\d]+)_(?P<Drug>[A-Za-z0-9]+)_(?P<Concentration>[\d\w_-]+uM)(?:.+Position_(?P<Position>[\d]))?",
+        SAVE_FIG=False,
+        SAVE_CSV=False,
+        bad_cols_file="",
+        bad_cols=BAD_COLS,
+        CHANNELS=1,
+        ZEROISCONTROL=True,
+        **kwargs,
+    ):
+        # sns.set()
+
+        np.random.seed(42)
+        # super(Cellprofiler,  self).__init__()
+        # self.df = pd.read_csv(nuclei_file_path)
+        # print(data_folder)
+        attrs = {
+            "data_folder": data_folder,
+            "nuclei_path": nuclei_path,
+            "image_path": image_path,
+            "organoid_path": organoid_path,
+            "SAVE_FIG": SAVE_FIG,
+            "SAVE_CSV": SAVE_CSV,
+            "bad_cols_file": bad_cols_file,
+            "bad_cols": bad_cols,
+            "CHANNELS": CHANNELS,
+            "ZEROISCONTROL": ZEROISCONTROL,
+            "object_headers": object_headers,
+            "filename_headers": filename_headers,
+            "regex_pattern": regex_pattern,
+            "index_headers": object_headers + filename_headers,
+        }
+        super(Cellprofiler, self).__init__(*args)
+        self.attrs = attrs
+
+        # print(self.attrs)
+        # df = self.get_df()
+        # super(Cellprofiler, self).__init__(*args,**kwargs)
+        # return CellprofilerActive
+        # super(Cellprofiler, self).__init__(,*args, **kwargs)
+        # self = pd.DataFrame(df)
+        # self.attrs.update(vars)
+        # self.update(df)
+
+        # print(df)
+        # self.index_headers = self.object_headers + self.filename_headers
+        # df = self.get_df()
+        #     # .pipe(self.extract_headers,regex_pattern,filename_headers,index_headers)
+        #     # .pipe(self.drop_bad_cols,bad_cols_file)
+        #     # .pipe(self.zero_is_control)
+        #     )
+
+        # self.update(df.to_dict())
+        # print(df)
+
+        # print(self)
+
+    def get_data(self):
+        # df = self.get_df(self.attrs["data_folder"],self.attrs["nuclei_path"])
+
+        df = (
+            self.get_df(self.attrs["data_folder"], self.attrs["nuclei_path"])
+            .pipe(
+                self.extract_headers,
+                self.attrs["regex_pattern"],
+                self.attrs["filename_headers"],
+                self.attrs["index_headers"],
+            )
+            .pipe(drop_bad_cols, self.attrs["bad_cols"])
+            .pipe(drop_bad_cols_by_file, self.attrs["bad_cols_file"])
+            .pipe(zero_is_control)
+            .pipe(drop_bad_index)
+            .pipe(force_numeric)
+        )
+        # df = CellprofilerDataFrame(df,attrs=self.attrs)
+
+        # df = CellprofilerDataFrame(df)
+        df.attrs.update(self.attrs)
+        # return CellprofilerDataFrame(df,attrs=self.attrs)
+        return df
+
+    @property
+    def _constructor(self):
+        # print(self.attrs)
+        # print("hello")
+        return Cellprofiler
+
+    def get_df(self, data_folder, nuclei_path):
+        # return pd.DataFrame()
+        # raw_df = self.read_data()
+        # extracted_data = self.extract_filenames(
+        #     self.get_filenames(raw_df),
+        #     regex_pattern,
+        #     filename_headers)
+        return self.read_data(data_folder, nuclei_path)
+
+    def read_data(self, data_folder, nuclei_path):
+        nuclei_df = pd.read_csv(os.path.join(data_folder, nuclei_path))
+        # if image_path is not None:
+        #     image_df = pd.read_csv(f"{data_folder}/{image_path}")
+        #     image_nuclei_df = pd.merge(
+        #         nuclei_df,
+        #         image_df,
+        #         on="ImageNumber",
+        #         how="left",
+        #         # left_index = True,
+        #         suffixes=("", "_Image"),
+        #     )
+        #     return image_nuclei_df
+        return nuclei_df
+
+    # %% Utils
+
+    def extract_headers(self, df, regex_pattern, filename_headers, index_headers):
+        filenames = self.get_filenames(df)
+        extracted_data = self.extract_filenames(
+            filenames, regex_pattern, filename_headers
+        )
+        df_indexed = df.join(extracted_data).set_index(index_headers)
+        return df_indexed.drop(
+            ["Metadata_FileLocation", "PathName_image"], axis=1, errors="ignore"
+        )
+
+    def get_filenames(self, df):
+        return df[
+            df.columns.intersection(["Metadata_FileLocation", "PathName_image"])
+        ].iloc[:, 0]
+
+    def extract_filenames(self, filenames, regex_pattern, filename_headers, decimals=3):
+        extracted_data = filenames.str.extract(regex_pattern)
+        extracted_data["filenames"] = filenames
+        extracted_data["Replicate"] = extracted_data["Position"].fillna(1)
+        extracted_data["Conc /uM"] = pd.to_numeric(
+            extracted_data["Concentration"]
+            .str.replace("_", ".")
+            .str.replace("-", ".")
+            .str.replace("uM", "")
+        ).round(decimals=decimals)
+        return extracted_data[filename_headers]
