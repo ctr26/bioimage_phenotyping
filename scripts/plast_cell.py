ccbca653b299363d5b3de632ee275493f7a4b15d
Craig Russell <ctr26@ebi.ac.uk>
2023-04-07 12:02:48 +0100

    Stashing changesc

--- scripts/plast_cell.py
+++ scripts/plast_cell.py
@@ -3,7 +3,12 @@ import PIL
 from PIL import Image
 import pims
 from glob import glob
+import matplotlib.pyplot as plt
 data_dir = "data"
+import bioimage_phenotyping as bip
+from bip.segmentation import Segmenter
+
+# Lif files are the brightfield images 
 ext = ".lif"
 glob_str = f"{data_dir}/**/*{ext}"
 files = glob(glob_str,recursive=True)
@@ -15,3 +20,25 @@ pims.bioformats.download_jar(version="6.7.0")
 
 print("ok")
 im = pims.Bioformats(files[0])
+
+# DAPI
+
+im.default_coords['c'] = 0
+
+
+
+# Brightfield
+
+im.default_coords['c'] = 1
+
+plt.imshow(im[0])
+
+image_2D = im[0]
+
+
+
+Segmenter()
+image_2D
+
+    
+
